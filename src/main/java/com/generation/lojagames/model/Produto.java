b8dbaa79f3db9778d71c5852471ae9f86cc4acfa
flrvia <flavixbatista@gmail.com>
2022-10-08 00:01:19 -0300

    Criação da Loja de Games

---
+++ src/main/java/com/generation/lojagames/model/Produto.java
@@ -0,0 +1,105 @@
+package com.generation.lojagames.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O atributo nome é obrigatório")
+	private String nome;
+	
+	@NotBlank(message = "O atributo marca é obrigatório")
+	private String marca;
+	
+	@NotBlank(message = "O atributo plataforma é obrigatório")
+	private String plataforma;
+	
+	@JsonFormat(shape = JsonFormat.Shape.STRING)
+    @NotNull(message = "Preço é obrigatório!")
+    @Positive(message = "O preço deve ser maior do que zero!")
+    private BigDecimal preco;
+	
+	@NotNull(message = "A quantidade é obrigatória")
+	private Integer quantidade;
+
+	@ManyToOne
+	@JsonIgnoreProperties("produtos")
+	private Categoria categoria;
+	
+	
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public String getPlataforma() {
+		return plataforma;
+	}
+
+	public void setPlataforma(String plataforma) {
+		this.plataforma = plataforma;
+	}
+	
+}
